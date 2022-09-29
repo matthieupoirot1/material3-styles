db47e531038c06b05a578169974b62277977bd51
Mathis Buchet <58893042+Barvoziker@users.noreply.github.com>
2022-09-29 09:43:30 +0200

    Casting 'findViewById(...)' to 'Toolbar' is redundant

--- app/src/main/java/edu/info/aen/first_test/MainActivity.java
+++ app/src/main/java/edu/info/aen/first_test/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+
 import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         this.cptTv = findViewById(R.id.tv_cpt);
         Button lessBtn = findViewById(R.id.btn_less);
